0e879e2a5ac13ab07a056928641068c39f689b47
selva <selvarudram@gmail.com>
2017-10-10 18:26:24 +0530

    Seond Commit

---
+++ routes/api.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('../config/database');
+var User = require('../app/models/user');
+
+
+var app = express();
+var router = express.Router();
+
+
+mongoose.connect(config.database);
+app.set('superSecret',config.secret);
+
+
+
+
+/*User sign up page. */
+router.post('/signup', function(req, res, next) {
+	 var userinfo = new User({
+	 	name: req.body.name,
+	 	email: req.body.email,
+	 	mobile: req.body.mobile,
+	 	password: req.body.password
+	 });
+	 userinfo.save(function (err,user) {
+	 	if (err) return JSON.stringify(err);
+	 	//saved
+	 	if(user) {
+	 		res.send(user);
+	 				
+		}	
+	 })
+	});
+
+
+// User Signin
+router.post('/login', function(req, res, next) {
+	User.findOne({"email":req.body.email,"password":req.body.password},function(err, Asignin){
+		if(Asignin) {
+			res.send({"message":"success"});
+		}else{
+			res.send({"message":"failure"});
+		}	
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
